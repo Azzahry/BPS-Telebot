8a8733ad27711e7d6e5288e520cd4f7a114bcad4
Azzahry <pram092003@gmail.com>
2024-04-04 08:53:13 +0700

    update main

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ async def content_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
     if 'bps 1' in text_diterima:
         await pst_menu(update)
     elif 'bps 2' in text_diterima:
-        await update.message.reply_photo(photo='image/logo-or-header/statistics-consultan.jpg', caption='Selamat Datang! \nPanduan Konsultasi Statistik Online & Offline 📲 \n\nPilih Layanan Konsultasi : \n1. Standar Pelayanan Rekomendasi Kegiatan Statistik Sektoral  \n2. Standar Pelayanan Perpustakaan  \n3. Standar Pelayanan Konsultasi Data Statistik  \n4. Standar Pelayanan Penjualan Produk BPS  \n5 Daftar Jenis & Tarif Penerimaan Negara Bukan Pajak BPS  \n\nUntuk memilih, silahkan ketik dengan format berikut : /ks (pilihan anda)\n Contoh : /ks 1')
+        await update.message.reply_photo(photo='image/logo-or-header/statistics-consultan.jpg', caption='Selamat Datang! \nPanduan Konsultasi Statistik Online & Offline 📲 \n----------------\nPilih Layanan Konsultasi : \n1. Standar Pelayanan Rekomendasi Kegiatan Statistik Sektoral  \n2. Standar Pelayanan Perpustakaan  \n3. Standar Pelayanan Konsultasi Data Statistik  \n4. Standar Pelayanan Penjualan Produk BPS  \n5 Daftar Jenis & Tarif Penerimaan Negara Bukan Pajak BPS  \n----------------\nUntuk memilih, silahkan ketik dengan format berikut : /ks (pilihan anda)\n Contoh : /ks 1')
         await konsultasi_statistik(update, context)
     elif 'bps 3' in text_diterima:
         await data_statistik(update)
@@ -142,13 +142,13 @@ async def content_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
     elif 'bps 5' in text_diterima:
         await saran_pengaduan(update)
     elif 'bps sk' in text_diterima:
-        await update.message.reply_photo(photo='image/logo-or-header/society.jpg',caption='Selamat Datang! \nSosial dan Kependudukan 👨‍👩‍👧‍👦 \n\nPilih Layanan Informasi : \n1. Agama\n2. Gender\n3. Geografi\n4. Iklim\n5. Indeks Pembangunan Manusia\n6. Kemiskinan dan Ketimpangan \n7. Kependudukan\n8. Kesehatan\n9. Konsumsi dan Pengeluaran\n10. Lingkungan Hidup\n11. Pemerintahan\n12. Pendidikan\n13. Perumahan\n14. Politik dan Keamanan\n15. Sosial Budaya\n16. Tenaga Kerja\n\nUntuk memilih, silahkan ketik dengan format berikut : /sk (pilihan anda)\n Contoh : /sk 1')
+        await update.message.reply_photo(photo='image/logo-or-header/society.jpg',caption='Selamat Datang! \nSosial dan Kependudukan 👨‍👩‍👧‍👦 \n----------------\nPilih Layanan Informasi : \n1. Agama\n2. Gender\n3. Geografi\n4. Iklim\n5. Indeks Pembangunan Manusia\n6. Kemiskinan dan Ketimpangan \n7. Kependudukan\n8. Kesehatan\n9. Konsumsi dan Pengeluaran\n10. Lingkungan Hidup\n11. Pemerintahan\n12. Pendidikan\n13. Perumahan\n14. Politik dan Keamanan\n15. Sosial Budaya\n16. Tenaga Kerja\n----------------\nUntuk memilih, silahkan ketik dengan format berikut : /sk (pilihan anda)\n Contoh : /sk 1')
         await sosial_kependudukan(update, context)
     elif 'bps ep' in text_diterima:
-        await update.message.reply_photo(photo='image/logo-or-header/economic-and-trading.jpg',caption='Selamat Datang! \nEkonomi dan Perdagangan      \n\nPilih Layanan Informasi : \n1. Ekspor-Impor\n2. Energi\n3. Harga Eceran\n4. Harga Produsen\n5. Industri\n6. Inflasi\n7. Keuangan\n8. Komunikasi\n9. Pariwisata\n10. Produk Domestik Regional Bruto\n11. Transportasi\n12. Upah Buruh\n\nUntuk memilih, silahkan ketik dengan format berikut : /ep (pilihan anda)\nContoh : /ep 1')
+        await update.message.reply_photo(photo='image/logo-or-header/economic-and-trading.jpg',caption='Selamat Datang! \nEkonomi dan Perdagangan      \n----------------\nPilih Layanan Informasi : \n1. Ekspor-Impor\n2. Energi\n3. Harga Eceran\n4. Harga Produsen\n5. Industri\n6. Inflasi\n7. Keuangan\n8. Komunikasi\n9. Pariwisata\n10. Produk Domestik Regional Bruto\n11. Transportasi\n12. Upah Buruh\n----------------\nUntuk memilih, silahkan ketik dengan format berikut : /ep (pilihan anda)\nContoh : /ep 1')
         await ekonomi_perdagangan(update, context)
     elif 'bps pp' in text_diterima:
-        await update.message.reply_photo(photo='image/logo-or-header/agriculture-and-mining.jpg', caption='Selamat Datang! \nEkonomi dan Perdagangan      \n\nPilih Layanan Informasi : \n1 Holtikultura\n2. Kehutanan\n3. Perikanan\n4. Perkebunan\n5. Pertambangan\n6. Peternakan\n7. Tanaman Pangan\n\nUntuk memilih, silahkan ketik dengan format berikut : /pp (pilihan anda)\nContoh : /pp 1')
+        await update.message.reply_photo(photo='image/logo-or-header/agriculture-and-mining.jpg', caption='Selamat Datang! \nEkonomi dan Perdagangan      \n----------------\nPilih Layanan Informasi : \n1 Holtikultura\n2. Kehutanan\n3. Perikanan\n4. Perkebunan\n5. Pertambangan\n6. Peternakan\n7. Tanaman Pangan\n----------------\nUntuk memilih, silahkan ketik dengan format berikut : /pp (pilihan anda)\nContoh : /pp 1')
         await pertanian_pertambangan(update, context)
     else:
         await update.message.reply_text('Pilihan tidak ada / valid. Silahkan ketikkan kembali pilihan anda (CONTENT COMMAND)')
